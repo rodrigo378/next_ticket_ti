ae93334ae520dbb60604853badd31378090e34a4
RODRIGO PALOMINO CASTRO <rodrigo.palomino@umared.com>
2025-08-18 17:30:10 -0500

    a

---
+++ src/app/(main)/ticket/[id]/page2.tsx
@@ -0,0 +1,5 @@
+import TicketDetalleView from "@/features/ticket-vir/usuario/detalleTicket/TicketDetalleView";
+
+export default function Page() {
+  return <TicketDetalleView></TicketDetalleView>;
+}
