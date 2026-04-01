db6c13b21deaefc2a54f1d3e1014ccbb243b4ccb
RODRIGO PALOMINO CASTRO <rodrigo.palomino@umared.com>
2026-04-01 10:56:25 -0500

    a

--- src/services/hd/catalogo.ts
+++ src/services/hd/catalogo.ts
@@ -3,20 +3,20 @@ import { api } from "../api";
 import { HD_CatalogoServicio } from "@interfaces/hd";
 
 export const getCatalogo = async (
-  area_id?: string
+  area_id?: string,
 ): Promise<HD_CatalogoServicio[]> => {
   const params = new URLSearchParams();
 
   if (area_id) params.append("area_id", area_id);
 
   const response: AxiosResponse<HD_CatalogoServicio[]> = await api.get(
-    `/hd/catalogo?${params}`
+    `/hd/catalogo?${params}`,
   );
   return response.data;
 };
 
 export const createCatalogo = async (data: HD_CatalogoServicio) => {
-  const response = await api.post("//hd/catalogo", data);
+  const response = await api.post("/hd/catalogo", data);
 
   return response.data;
 };
